37d82dc17a885140805ab5b2f784a305646e2cc1
hyewoncc <hyewoncc@gmail.com>
2022-08-24 16:49:51 +0900

    build: Ability 스프링 마이그레이션 프로젝트 초기화

---
+++ src/test/java/com/ability/AbilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
